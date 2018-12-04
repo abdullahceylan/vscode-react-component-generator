684ec43ffd1005ae9c068684bba5bec00730c880
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-12-04 00:30:45 +0000

    Update CSSInterface

--- src/interfaces/types/index.ts
+++ src/interfaces/types/index.ts
@@ -1,15 +1,15 @@
 export interface BaseInterface {
   create: boolean,
   extension: string,
-  template?: string,
-  suffix?: string,
-  type?: string
+  template?: string
 }
 
 export interface ComponentInterface extends BaseInterface {
 }
 
 export interface CSSInterface extends BaseInterface {
+  suffix?: string,
+  type?: string
 }
 
 export interface IndexInterface extends BaseInterface {
